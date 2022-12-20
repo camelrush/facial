c0dd5d5650a7c1ac9fcbc3c290f6488f251a0ec6
s.takada.ksap.labo <s.takada.ksap.labo@gmail.com>
2022-12-20 22:53:03 +0900

    comment update

--- 04_train_model.py
+++ 04_train_model.py
@@ -24,7 +24,6 @@ for (i, imagePath) in enumerate(imagePaths):
 
     # RGB画像から顔を検出し、顔の座標を得る(CNNを使用)
     boxes = face_recognition.face_locations(rgb, model="cnn")
-    print(len(boxes))
 
     # 顔座標の矩形画像（つまり顔写真）を、数値リストに変換
     encodings = face_recognition.face_encodings(rgb, boxes)
